aa93d403afb3e883ed215b006c9d334e058d89c0
Matt Woods <mw@mattwoods.io>
2018-07-10 21:16:17 -0500

    updated styling and added tweet intent

--- src/App.js
+++ src/App.js
@@ -37,12 +37,20 @@ class App extends Component {
     return (
       <QuoteBoxWrapper id="quote-box">
         <QuoteBoxChild>
-          <p id="text">{this.state.quote}</p>
+          <h1 id="text">{this.state.quote}</h1>
           <p id="author">{this.state.author}</p>
           <Button id="new-quote" onClick={this.fetchQuote}>
             New Quote
           </Button>
-          <Button id="tweet-quote">Tweet Quote</Button>
+          <Button id="tweet-quote">
+            <a
+              href={`https://twitter.com/intent/tweet?text="${
+                this.state.quote
+              }"%20\n\n-${this.state.author}`}
+            >
+              Tweet Quote
+            </a>
+          </Button>
         </QuoteBoxChild>
       </QuoteBoxWrapper>
     );
@@ -55,11 +63,16 @@ const QuoteBoxWrapper = styled.div`
   align-items: center;
   justify-content: center;
   height: 100vh;
+  font-size: 1.25em;
+  font-weight: 400;
+  color: hsl(196, 95%, 12%);
+  background-color: #b9e1f0;
+  background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%230793ca' fill-opacity='0.1'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 `;
 
 const QuoteBoxChild = styled.div`
-  font-size: 2em;
   padding: 1em;
+  max-width: 700px;
 `;
 
 const Button = styled.button`
@@ -78,6 +91,15 @@ const Button = styled.button`
   color: #fff;
   text-decoration: none;
   transition: all 0.15s ease;
+  &:hover {
+    cursor: pointer;
+    box-shadow: 0 15px 35px rgba(50, 50, 93, 0.1),
+      0 5px 15px rgba(0, 0, 0, 0.07);
+  }
+  a {
+    text-decoration: none;
+    color: #fff;
+  }
 `;
 
 export default App;
